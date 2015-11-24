44bbb47bdd00dd3afcd28bf0e4af77971f6f5af8
cfc1020 <cfc9209@gmail.com>
2015-11-24 13:50:55 +0300

    Add Campaign overview.

--- app/facades/report_template.rb
+++ app/facades/report_template.rb
@@ -9,7 +9,17 @@ class ReportTemplate
     @advertiser_report_items_group_by_campaign_and_day ||= advertiser_report_items.group_by(&:date)
   end
 
-  def 
+  # for all fields :)
+  def campaign_totals
+    @campaign_totals ||= advertiser_report_items.inject({}) do |memory, advertiser_report_item|
+      advertiser_report_item.to_h.each do |key, value|
+        memory[key] ||= 0.0
+        memory[key] += value.to_f
+      end
+
+      memory
+    end
+  end
 
   def creatives
     # It's unclear how to deal with the following error: interrupt and repeat everything or skip
